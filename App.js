8bd6304848f00341869f6726e695360e372bece6
otter0104 <otter0104@innodep.com>
2022-07-14 17:26:45 +0900

    drawer navigation

--- App.js
+++ App.js
@@ -7,9 +7,10 @@
  */
 
 import React, {useState} from 'react';
-import {Button, StyleSheet, TextInput, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeScreen from './src/screen/Home';
 import DetailsScreen from './src/screen/Details';
 import MainScreen from './src/screen/Main';
@@ -40,14 +41,25 @@ const CreatePostScreen = ({navigation}) => {
   );
 };
 
+const Drawer = createDrawerNavigator();
+
+const MenuScrren = () => {
+  return (
+    <View>
+      <Text>Menu</Text>
+    </View>
+  );
+};
+
 const App = () => {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
+      {/* <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Menu" component={MenuScrren} />
+      </Drawer.Navigator> */}
       <Stack.Navigator initialRouteName="Home">
-        {/* <Stack.Screen name="Drawer" component={DrawerScreen} /> */}
-        {/* <RightDrawer /> */}
         <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="Screen1" component={Screen1} />
         <Stack.Screen name="Screen2" component={Screen2} />
